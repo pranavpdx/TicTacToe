153a168d0043a28b70cff714fa33d7e1e9fae54e
pranavpdx <pranavpdx@gmail.com>
2019-02-04 21:42:55 -0800

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,14 @@
+/*
+ * This program plays a TicTacToe game with 2 users in the console 
+ * Date: 9/16/18
+ * Author: Pranav Sharma
+ */
 import java.util.Scanner;
 
 public class TicTacToe {
+	// creates an array that represents the board that players play on
 	int[][] board = new int[3][3];
+	// Instantiation of all the variables that will be used later in the code
 	final int BLANK = 0;
 	final int X_MOVE = 1;
 	final int O_MOVE = 2;
@@ -13,6 +20,8 @@ public class TicTacToe {
 	int xWins = 0;
 	int oWins = 0;
 
+	// Constructor that runs that code that interacts with the players and takes
+	// their inputs
 	public TicTacToe() {
 		boolean stillPlaying = true;
 		scanner = new Scanner(System.in);
@@ -21,20 +30,27 @@ public class TicTacToe {
 		String player1 = scanner.nextLine();
 		System.out.println("Enter player 2's name (This player will be O): ");
 		String player2 = scanner.nextLine();
+		// Only runs the code while the user says they want to keep playing. Asks after
+		// each game is completed
 		while (stillPlaying == true) {
-			System.out.println("Ok! Lets play! Its " +player1 + "'s turn:");
+			System.out.println("Ok! Lets play! Its " + player1 + "'s turn:");
 			printBoard();
+			// Game keeps running while no player has won and there are no ties
 			while (checkWin(X_MOVE) == false && checkWin(O_MOVE) == false && checkTie() == false) {
 				input = scanner.nextLine();
+				// These If and Else if statements check to see that the move the user inputs is
+				// a valid move on the board
 				if (input.length() != 2) {
-					System.out.println("Enter a letter(a, b, or c) followed by a number (1, 2, or 3)");
+					System.out.println("Enter a letter(a, b, or c) followed by a number(1, 2, or 3)");
 				} else if (input.charAt(0) != 'a' && input.charAt(0) != 'b' && input.charAt(0) != 'c') {
 					System.out.println("Row must be an a, b, or c");
 				} else if (input.charAt(1) != '1' && input.charAt(1) != '2' && input.charAt(1) != '3') {
 					System.out.println("colomn must be a 1, 2, or 3");
 				} else {
+					// If input is valid runs actual code to place and X or an O on the board
 					int row = input.charAt(0) - 'a';
 					int colomn = input.charAt(1) - '1';
+					// Makes sure the space the user wants to place an X or an O is blank
 					if (board[row][colomn] == BLANK) {
 						if (turn == X_TURN) {
 							board[row][colomn] = X_MOVE;
@@ -47,26 +63,30 @@ public class TicTacToe {
 						System.out.println("Sorry! There's already an X or an O there");
 					}
 					printBoard();
-					if( turn == O_TURN && checkWin(X_MOVE) == false && checkWin(O_MOVE) == false 
-						&& checkTie() == false ) {
+					// Prints which players turn it is
+					if (turn == O_TURN && checkWin(X_MOVE) == false && checkWin(O_MOVE) == false
+							&& checkTie() == false) {
 						System.out.println(player2 + "'s turn:");
-					}else if (turn == X_TURN && checkWin(X_MOVE) == false && checkWin(O_MOVE) == false 
-							  && checkTie() == false ) {
+					} else if (turn == X_TURN && checkWin(X_MOVE) == false && checkWin(O_MOVE) == false
+							&& checkTie() == false) {
 						System.out.println(player1 + "'s turn:");
 					}
 				}
+				// Checks if a player has won and if so prints which player won and how many
+				// times
 				if (checkWin(X_MOVE) == true) {
 					System.out.println("Congratulations! " + player1 + " wins");
-					xWins ++;
+					xWins++;
 					System.out.println(player1 + " has won " + xWins + " times!");
-				} else if(checkWin(O_MOVE) == true) {
+				} else if (checkWin(O_MOVE) == true) {
 					System.out.println("Congratulations! " + player2 + " wins");
 					oWins++;
 					System.out.println(player2 + " has won " + oWins + " times!");
-				} else if( checkTie() == true) {
+				} else if (checkTie() == true) {
 					System.out.println("Its a tie!");
 				}
 			}
+			// Asks if users would like to play again
 			System.out.println("Would you like to play again?");
 			String input2 = scanner.nextLine();
 			if (input2.equals("yes") || input2.equals("y") || input2.equals("Yes")) {
@@ -77,17 +97,21 @@ public class TicTacToe {
 					}
 				}
 			} else {
+				// Ends program if users don't want to continue playing
 				stillPlaying = false;
 				System.out.println("Thank you for playing");
 			}
 		}
 	}
 
+	// Main method that runs the constructor
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new TicTacToe();
 	}
 
+	// Method that prints out the board at the start, as well as after the user
+	// enters an X or an O to put on the board
 	public void printBoard() {
 		System.out.println(" \t1\t2\t3");
 		for (int row = 0; row < board.length; row++) {
@@ -105,6 +129,8 @@ public class TicTacToe {
 		}
 	}
 
+	// Method that checks each possible win on the board to check to see if a player
+	// has won yet, returns a true or false
 	public boolean checkWin(int player) {
 		if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
 			return true;
@@ -129,24 +155,26 @@ public class TicTacToe {
 		}
 		if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
 			return true;
+		} else {
+			return false;
 		}
-		else{return false;}
 	}
 
+	// this method checks to see if all the board sports are filled, if so it
+	// returns a "true" for a tie, "false" for no tie
 	public boolean checkTie() {
 		int x = 0;
 		for (int row = 0; row < board.length; row++) {
 			for (int colomn = 0; colomn < board[0].length; colomn++) {
 				if (board[row][colomn] != BLANK) {
-					x = x+1;
+					x = x + 1;
 				}
 			}
 		}
-		if( x == 9) {
+		if (x == 9) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
 	}
 }
